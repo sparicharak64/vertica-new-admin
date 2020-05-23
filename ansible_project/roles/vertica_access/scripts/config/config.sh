ed820697228a6e7082c427754b77f42954a968b4
Sushant <mm37864@massmutual.com>
2020-05-23 19:56:39 -0400

    Host file commit

---
+++ ansible_project/roles/vertica_access/scripts/config/config.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+export VERTICA_HOST=${AWS_VERTICA_QA_EON}
+export VERTICA_USER=mm37864
+export VERTICA_PASSWORD=password 
+export DATABASE=advana
+export PORT=443
--- ansible_project/roles/vertica_access/scripts/schemas.sh
+++ ansible_project/roles/vertica_access/scripts/schemas.sh
@@ -1,1 +1,5 @@
-#!/usr/bin/sh
+#!/bin/bash
+
+echo "Connect to vertica using below variables"
+
+export VERTICA_HOST
