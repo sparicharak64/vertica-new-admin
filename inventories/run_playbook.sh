034ee05230ef31279fc0f59e8abbb15f82d22845
Sushant <mm37864@massmutual.com>
2020-05-24 06:48:28 -0400

    Update run_playbook.sh

--- inventories/run_playbook.sh
+++ inventories/run_playbook.sh
@@ -2,5 +2,5 @@ set -e
 set -x
 
 ansible-playbook -f 9 inventories/provisionaccess.yml \
-    --extra-vars "@group_vars/all" \
+    --extra-vars "group_vars/all" \
     --vault-password-file ./.vault-pw.txt
