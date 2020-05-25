86069be53e42e6f7d966305f4b4525a98f3d8f6c
Sushant <mm37864@massmutual.com>
2020-05-24 22:43:06 -0400

    Update run_playbook.sh

--- inventories/run_playbook.sh
+++ inventories/run_playbook.sh
@@ -3,4 +3,5 @@ set -x
 
 ansible-playbook -f 10 -f 9 $1 "${@:2}" \
     --extra-vars "@group_vars/all" \
-    --vault-password-file ./.vault-pw.txt
+    --vault-password-file ./.vault-pw.txt \
+    --limit master
